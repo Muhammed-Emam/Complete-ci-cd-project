f953c8cedac8c27dc1bd23db83a975a41d53a66d
MU <you@example.com>
2023-05-31 19:48:41 +0300

    added ansible

--- terraform/modules/eks/node-group.tf
+++ terraform/modules/eks/node-group.tf
@@ -16,6 +16,6 @@ resource "aws_eks_node_group" "worker-node-group" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.EC2InstanceProfileForImageBuilderECRContainerBuilds,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
-
+    
    ]
 }
